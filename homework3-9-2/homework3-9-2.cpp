d219b94d93f886756b092296c27b729f06dfea1a
Ersyn <akhmetov.ersyn@gmail.com>
2023-03-25 21:02:36 +0600

    Добавьте файлы проекта.

---
+++ homework3-9-2/homework3-9-2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	int multiplyFirstCross(Fraction other) { return numerator_ * other.denominator_; }
+	int multiplySecondCross(Fraction other) { return other.numerator_ * denominator_; }
+
+	bool operator==(Fraction other) { return numerator_ == other.numerator_ && denominator_ == other.denominator_; }
+	bool operator!=(Fraction other) { return !(numerator_ == other.numerator_ && denominator_ == other.denominator_); }
+	bool operator<(Fraction other) { return multiplyFirstCross(other) < multiplySecondCross(other); }
+	bool operator>(Fraction other) { return multiplyFirstCross(other) > multiplySecondCross(other); }
+	bool operator<=(Fraction other) { return !(multiplyFirstCross(other) > multiplySecondCross(other)); }
+	bool operator>=(Fraction other) { return !(multiplyFirstCross(other) < multiplySecondCross(other)); }
+
+
+};
+
+int main()
+{
+	Fraction f1(5, 3);
+	Fraction f2(4, 3);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+
+	return 0;
+}
